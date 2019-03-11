51aee16190ec0e05ec9c41c973bf605d5fc76371
anil <anilravulapalli20@gmail.com>
2019-03-11 12:23:23 +0530

    added the other classes

---
+++ ServerResponse/src/main/java/com/sr/dto/RideInfo.java
@@ -0,0 +1,40 @@
+package com.sr.dto;
+
+public class RideInfo {
+	protected String rideNo;
+	protected String cabNo;
+	protected String driverName;
+	protected String estimatedPickup;
+	protected String mobileNo;
+	public String getRideNo() {
+		return rideNo;
+	}
+	public void setRideNo(String rideNo) {
+		this.rideNo = rideNo;
+	}
+	public String getCabNo() {
+		return cabNo;
+	}
+	public void setCabNo(String cabNo) {
+		this.cabNo = cabNo;
+	}
+	public String getDriverName() {
+		return driverName;
+	}
+	public void setDriverName(String driverName) {
+		this.driverName = driverName;
+	}
+	public String getEstimatedPickup() {
+		return estimatedPickup;
+	}
+	public void setEstimatedPickup(String estimatedPickup) {
+		this.estimatedPickup = estimatedPickup;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
+}
---
+++ ServerResponse/src/main/java/com/sr/dto/RideRequest.java
@@ -0,0 +1,33 @@
+package com.sr.dto;
+
+public class RideRequest {
+	protected String source;
+	protected String destination;
+	protected String cabType;
+	protected String bookingType;
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public String getCabType() {
+		return cabType;
+	}
+	public void setCabType(String cabType) {
+		this.cabType = cabType;
+	}
+	public String getBookingType() {
+		return bookingType;
+	}
+	public void setBookingType(String bookingType) {
+		this.bookingType = bookingType;
+	}
+	
+}
